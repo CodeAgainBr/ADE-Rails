85d8e3e349cfaf49a0ff126f2c51564563d8001c
alexrms <ricdoa2@gmail.com>
2018-07-28 22:12:35 -0300

    Change archive of local Date Time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def index
+		Time.zone = "America/Sao_Paulo"
 		@data = Date.current
 
 		@jogo = Jogo.where("data >= ?", @data).order(:data).limit(1)[0]
