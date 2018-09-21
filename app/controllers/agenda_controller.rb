a6a27ec417f666bb8526bc2650e1301bf7fea489
alexrms <ricdoa2@gmail.com>
2018-09-20 22:38:27 -0300

    Create 'Agenda Telefonica'

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -17,4 +17,22 @@ class AgendaController < ApplicationController
       @i += 1
     end
   end
+
+  # GET /agenda/telefonica
+  def index
+    @data = Date.current
+    
+    @meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outrubro', 'Novembro', 'Dezembro']
+
+    @jogos_ano = []
+    @i = 1
+    while @i <= 12
+      @jogos = Jogo.where("extract(month from data)  = ? and extract(year from data) = ?", @i.to_s, @data.strftime("%Y")).order(:data)
+      @jogos.each do |jogo|
+        jogo.clube = Clube.find(jogo.clube)
+      end
+      @jogos_ano.push(@jogos)
+      @i += 1
+    end
+  end
 end
