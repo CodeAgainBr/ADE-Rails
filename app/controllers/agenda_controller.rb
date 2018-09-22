863f1c413fd6962067ae782dfcc942744652db3f
alexrms <ricdoa2@gmail.com>
2018-09-21 21:49:54 -0300

    Changing view 'agenda/telefonica'

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -19,20 +19,7 @@ class AgendaController < ApplicationController
   end
 
   # GET /agenda/telefonica
-  def index
-    @data = Date.current
-    
-    @meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outrubro', 'Novembro', 'Dezembro']
-
-    @jogos_ano = []
-    @i = 1
-    while @i <= 12
-      @jogos = Jogo.where("extract(month from data)  = ? and extract(year from data) = ?", @i.to_s, @data.strftime("%Y")).order(:data)
-      @jogos.each do |jogo|
-        jogo.clube = Clube.find(jogo.clube)
-      end
-      @jogos_ano.push(@jogos)
-      @i += 1
-    end
+  def telefonica
+    @associados = Associado.all.order(:nome)
   end
 end
