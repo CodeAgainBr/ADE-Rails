6c4203d3ecea56abec24a95e5d62153e62f2e636
GilmarBec <trabalhos4.5.1.4@gmail.com>
2019-01-23 23:01:34 -0200

    configs js compressor (herko requirements)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,13 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier #Original
   # config.assets.css_compressor = :sass
-
+  
+  # Compress JavaScript and CSS.(Novo)
+  config.assets.uglifier = { harmony: true }
+  config.assets.js_compressor = :uglifier_with_source_maps
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
