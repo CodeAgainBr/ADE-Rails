08590b7b9666337204be45338171d2bfa5bf29ad
alexrms <ricdoa2@gmail.com>
2019-02-23 14:11:22 -0300

    Fix show empty tables in 'relatorio de aniversários'

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -1,71 +1,74 @@
 class RelatoriosController < ApplicationController
 
-	# GET /relatorios
-	def index
-	end
+  # GET /relatorios
+  def index
+  end
 
   #GET /relatorios/aniversario
   #GET /relatorios/aniversario/pdf
   def aniversario
-
   end
 
   def aniversario_pdf
-    if params[:referente] == "geral"
-      if params[:tipo] == "anual"
+    if params[:referente] == 'geral'
+      if params[:tipo] == 'anual'
         @associados = Associado.all.order(:nome)
         @parentes = Parente.all.order(:nome)
         @clubes = Clube.all.order(:nome)
-      elsif params[:tipo] == "mensal"
-        @associados = Associado.where("extract(month from data_nascimento)  = ?", params[:data])
-        @parentes = Parente.where("extract(month from data_nascimento)  = ?", params[:data])
-        @clubes = Clube.where("extract(month from data_fundacao)  = ?", params[:data])
-      elsif params[:tipo] == "diario"
-        @associados = Associado.where("extract(day from data_nascimento)  = ? and extract(month from data_nascimento)  = ?", params[:data].split("/")[0], params[:data].split("/")[1])
-        @parentes = Parente.where("extract(day from data_nascimento)  = ? and extract(month from data_nascimento)  = ?", params[:data].split("/")[0], params[:data].split("/")[1])
-        @clubes = Clube.where("extract(day from data_fundacao)  = ? and extract(month from data_fundacao)  = ?", params[:data].split("/")[0], params[:data].split("/")[1])
       end
-    elsif params[:referente] == "associados"
-      if params[:tipo] == "anual"
+
+      if params[:tipo] == 'mensal'
+        @associados = Associado.where('extract(month from data_nascimento)  = ?', params[:data])
+        @parentes = Parente.where('extract(month from data_nascimento)  = ?', params[:data])
+        @clubes = Clube.where('extract(month from data_fundacao)  = ?', params[:data])
+      end
+
+      if params[:tipo] == 'diario'
+        @associados = Associado.where('extract(day from data_nascimento)  = ? and extract(month from data_nascimento)  = ?', params[:data].split('/')[0], params[:data].split('/')[1])
+        @parentes = Parente.where('extract(day from data_nascimento)  = ? and extract(month from data_nascimento)  = ?', params[:data].split('/')[0], params[:data].split('/')[1])
+        @clubes = Clube.where('extract(day from data_fundacao)  = ? and extract(month from data_fundacao)  = ?', params[:data].split('/')[0], params[:data].split('/')[1])
+      end
+    end
+
+    if params[:referente] == 'associados'
+      if params[:tipo] == 'anual'
         @associados = Associado.all.order(:nome)
-        @parentes = []
-        @clubes = []
-      elsif params[:tipo] == "mensal"
-        @associados = Associado.where("extract(month from data_nascimento)  = ?", params[:data])
-        @parentes = []
-        @clubes = []
-      elsif params[:tipo] == "diario"
-        @associados = Associado.where("extract(day from data_nascimento)  = ? and extract(month from data_nascimento)  = ?", params[:data].split("/")[0], params[:data].split("/")[1])
-        @parentes = []
-        @clubes = []
       end
-    elsif params[:referente] == "parentes"
-      if params[:tipo] == "anual"
-        @associados = []
+
+      if params[:tipo] == 'mensal'
+        @associados = Associado.where('extract(month from data_nascimento)  = ?', params[:data])
+      end
+
+      if params[:tipo] == 'diario'
+        @associados = Associado.where('extract(day from data_nascimento)  = ? and extract(month from data_nascimento)  = ?', params[:data].split('/')[0], params[:data].split('/')[1])
+      end
+    end
+
+    if params[:referente] == 'parentes'
+      if params[:tipo] == 'anual'
         @parentes = Parente.all.order(:nome)
-        @clubes = []
-      elsif params[:tipo] == "mensal"
-        @associados = []
-        @parentes = Parente.where("extract(month from data_nascimento)  = ?", params[:data])
-        @clubes = []
-      elsif params[:tipo] == "diario"
-        @associados = []
-        @parentes = Parente.where("extract(day from data_nascimento)  = ? and extract(month from data_nascimento)  = ?", params[:data].split("/")[0], params[:data].split("/")[1])
-        @clubes = []
       end
-    elsif params[:referente] == "clubes"
-      if params[:tipo] == "anual"
-        @associados = []
-        @parentes = []
+
+      if params[:tipo] == 'mensal'
+        @parentes = Parente.where('extract(month from data_nascimento)  = ?', params[:data])
+      end
+
+      if params[:tipo] == 'diario'
+        @parentes = Parente.where('extract(day from data_nascimento)  = ? and extract(month from data_nascimento)  = ?', params[:data].split('/')[0], params[:data].split('/')[1])
+      end
+    end
+
+    if params[:referente] == 'clubes'
+      if params[:tipo] == 'anual'
         @clubes = Clube.all.order(:nome)
-      elsif params[:tipo] == "mensal"
-        @associados = []
-        @parentes = []
-        @clubes = Clube.where("extract(month from data_fundacao)  = ?", params[:data])
-      elsif params[:tipo] == "diario"
-        @associados = []
-        @parentes = []
-        @clubes = Clube.where("extract(day from data_fundacao)  = ? and extract(month from data_fundacao)  = ?", params[:data].split("/")[0], params[:data].split("/")[1])
+      end
+
+      if params[:tipo] == 'mensal'
+        @clubes = Clube.where('extract(month from data_fundacao)  = ?', params[:data])
+      end
+
+      if params[:tipo] == 'diario'
+        @clubes = Clube.where('extract(day from data_fundacao)  = ? and extract(month from data_fundacao)  = ?', params[:data].split('/')[0], params[:data].split('/')[1])
       end
     end
   end
