95ba3264d58a343eae4b2292badbe9a99222b5ee
GilmarBec <trabalhos4.5.1.4@gmail.com>
2019-04-12 14:49:09 -0300

    Update mask of 'telefone'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 $(document).on('turbolinks:load', function() {
 	$('.data').mask('00/00/0000');
 	$('.celular').mask('(00) 00000-0000');
-	$('.telefone').mask('(00) 0000-0000');
+	$('.telefone').mask('(00) 00000-0000');
+	//$('.telefone').mask('(00) 0000-0000');
 	$('.cep').mask('00000-000');
 	$('.rg').mask('0.000.000');
 	$('.cpf').mask('000.000.000-00');
